18418def3c8b130cb155b13e77f5c3f37b658ac6
orlando <75084509+dehoisted@users.noreply.github.com>
2021-07-15 18:33:37 -0400

    Add files via upload

---
+++ source/config.lua
@@ -0,0 +1,25 @@
+--[[
+	Simple Chat Spy
+	Type "spy" to enable or disable the chat spy.
+	Only tested if this works executed with Synapse (should work with other exploits though)
+--]]
+
+print("-- Chat Spy Executed --")
+print("Type \"spy\" to enable or disable the chat spy.")
+print("Only tested if this works executed with Synapse (should work with other exploits though)")
+print("https://github.com/dehoisted/Chat-Spy")
+
+-- Configuration
+Config = {
+	enabled = true,
+	spyOnMyself = true,
+	public = false,
+	publicItalics = true
+}
+
+-- Customizing Log Output
+PrivateProperties = {
+	Color = Color3.fromRGB(0,255,255); 
+	Font = Enum.Font.SourceSansBold;
+	TextSize = 18;
+}
