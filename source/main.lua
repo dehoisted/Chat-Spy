7cd95bbfcd906d94c9f485239dbf90c5d1865cfd
orlando <75084509+dehoisted@users.noreply.github.com>
2021-07-15 18:34:17 -0400

    Main File to Execute

--- source/main.lua
+++ source/main.lua
@@ -2,7 +2,7 @@
 	Simple Chat Spy
 	Type "spy" to enable or disable the chat spy.
 	Only tested if this works executed with Synapse (should work with other exploits though)
---]]
+]]
 
 print("-- Chat Spy Executed --")
 print("Type \"spy\" to enable or disable the chat spy.")
